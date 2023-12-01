21f8e17d33a349955327d2f6d4801535076fcd7f
raymondYsheng <bobisabob82@gmail.com>
2023-12-01 12:24:29 -0800

    inheritance and abstraction

--- src/main/java/com/nighthawk/spring_portfolio/mvc/crypto/Fibonacci.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/crypto/Fibonacci.java
@@ -1,33 +1,54 @@
-package com.nighthawk.spring_portfolio.mvc.crypto;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/fibonacci")
-public class Fibonacci {
+public abstract class Fibonacci {
 
-    @GetMapping("/{n}")
-    public long calculateFibonacci(@PathVariable int n) {
-        return fibonacci(n);
-    }
+    public abstract long calculateFibonacci(int n);
+}
 
-    @GetMapping("/sequence")
-    public long[] getFibonacciSequence(@RequestParam int start, @RequestParam int count) {
-        long[] sequence = new long[count];
-        for (int i = 0; i < count; i++) {
-            sequence[i] = fibonacci(start + i);
-        }
-        return sequence;
-    }
+class RecursiveFibonacciCalculator extends Fibonacci {
 
-    private long fibonacci(int n) {
+    @Override
+    public long calculateFibonacci(int n) {
         if (n <= 1) {
             return n;
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+    }
+}
+
+class IterativeFibonacciCalculator extends Fibonacci {
+
+    @Override
+    public long calculateFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        long fib = 1;
+        long prev = 0;
+        for (int i = 2; i <= n; i++) {
+            long temp = fib;
+            fib = fib + prev;
+            prev = temp;
+        }
+        return fib;
+    }
+}
+
+@RestController
+@RequestMapping("/api/fibonacci")
+class FibonacciController {
+
+    private final Fibonacci fibonacciCalculator;
+
+    public FibonacciController(Fibonacci fibonacciCalculator) {
+        this.fibonacciCalculator = fibonacciCalculator;
+    }
+
+    @GetMapping("/{n}")
+    public long calculateFibonacci(@PathVariable int n) {
+        return fibonacciCalculator.calculateFibonacci(n);
     }
 }
