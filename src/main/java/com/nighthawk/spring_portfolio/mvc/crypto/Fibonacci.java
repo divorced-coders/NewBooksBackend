73471247642613dfc4d5b6bf3e7104ad69e58a6c
raymondYsheng <bobisabob82@gmail.com>
2023-12-01 12:25:13 -0800

    package

--- src/main/java/com/nighthawk/spring_portfolio/mvc/crypto/Fibonacci.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/crypto/Fibonacci.java
@@ -1,3 +1,5 @@
+package com.nighthawk.spring_portfolio.mvc.crypto;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
